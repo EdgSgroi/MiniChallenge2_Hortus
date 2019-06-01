6ae375cb11a48544dfa298d9ac63273064dff438
Edgar Sgroi <edgarsgroi@Edgars-MacBook-Pro-3.local>
2019-06-01 18:30:38 -0300

    Master reset

--- MiniChallenge2_Hortus/ViewController.swift
+++ MiniChallenge2_Hortus/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  MiniChallenge2_Hortus
 //
-//  Created by Cassia Aparecida Barbosa on 09/05/19.
+//  Created by Cassia Aparecida Barbosa on 14/05/19.
 //  Copyright © 2019 Hortus. All rights reserved.
 //
 
@@ -12,9 +12,19 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
+    
 
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
